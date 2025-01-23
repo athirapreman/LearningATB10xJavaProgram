5dcee8488332843856123a1dd2ac6beb7ba498bb
Your Nameathirapreman <athirapremk@gmail.com>
2025-01-23 21:00:33 +0530

    23 jan

---
+++ src/ex_04_labexercise_Typecasting/Lab0022_Implicittpecasting.java
@@ -0,0 +1,9 @@
+package ex_04_labexercise_Typecasting;
+
+public class Lab0022_Implicittpecasting {
+    public static void main(String[] args) {
+        int a=10;
+        long b =a;
+        System.out.println(b);
+    }
+}
---
+++ src/ex_04_labexercise_Typecasting/Lab0023_explicittpecasting.java
@@ -0,0 +1,9 @@
+package ex_04_labexercise_Typecasting;
+
+public class Lab0023_explicittpecasting {
+    public static void main(String[] args) {
+        int a=1001;
+        byte b=(byte) a;
+        System.out.println(b);
+    }
+}
