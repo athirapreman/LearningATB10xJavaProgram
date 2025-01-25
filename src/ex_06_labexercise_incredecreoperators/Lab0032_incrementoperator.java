7a20683f809d5dcbb6c1b50b23341038b5661056
Your Nameathirapreman <athirapremk@gmail.com>
2025-01-25 18:58:21 +0530

    23 jan

---
+++ src/ex_06_labexercise_incredecreoperators/Lab0032_incrementoperator.java
@@ -0,0 +1,11 @@
+package ex_06_labexercise_incredecreoperators;
+
+public class Lab0032_incrementoperator {
+    public static void main(String[] args) {
+        int a = 10;
+
+        System.out.println(++a + a++ + a++);
+
+        System.out.println(a);
+    }
+}
---
+++ src/ex_06_labexercise_incredecreoperators/Lab0033_incrementoperator2nd.java
@@ -0,0 +1,9 @@
+package ex_06_labexercise_incredecreoperators;
+
+public class Lab0033_incrementoperator2nd {
+    public static void main(String[] args) {
+        int a = 20;
+
+        System.out.println(--a + a++ + a--);
+    }
+}
