70dd4843912acfec696284c4e09ef16a69f94f23
Your Nameathirapreman <athirapremk@gmail.com>
2025-01-25 19:37:03 +0530

    23 jan

--- src/ex_05_labexcercises_Ternaryoperator/Lab0031_Ternaryoperatormultiinputfromuser.java
+++ src/ex_05_labexcercises_Ternaryoperator/Lab0031_Ternaryoperatormultiinputfromuser.java
@@ -1,4 +1,14 @@
 package ex_05_labexcercises_Ternaryoperator;
 
 public class Lab0031_Ternaryoperatormultiinputfromuser {
+    public static void main(String[] args) {
+        String name = args[0];
+
+        String age_string = args[1];
+        int age = Integer.parseInt(age_string);
+        String salary_string = args[2];
+        int salary = Integer.parseInt(salary_string);
+
+        System.out.println("Name- "+name +" "+"\nAge-"+ age +"\nSalary- " +salary);
+    }
 }
