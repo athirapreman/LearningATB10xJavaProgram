2158dcfba743fc86b94d30503a2debf46550cf8a
Your Nameathirapreman <athirapremk@gmail.com>
2025-01-29 20:41:31 +0530

    23 jan

---
+++ src/ex_13_labaskforloop/Lab0049_labtaskforloop.java
@@ -0,0 +1,25 @@
+package ex_13_labaskforloop;
+
+public class Lab0049_labtaskforloop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=100 ; i++) {
+             if (i%3==0&&i%5==0) {
+                System.out.println("fizzbuzz");
+            }
+            else if(i%3==0) {
+                System.out.println("fizz");
+            } else if (i%5==0) {
+                System.out.println("buzz");
+            }
+
+            else {
+                System.out.println(i);
+            }
+        }
+
+        }
+    }
+
+
+
